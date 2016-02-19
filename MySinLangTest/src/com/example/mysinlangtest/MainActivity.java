e557816c666272749a988f34a2b5bc19cf721c33
gaoning <15981997516@163.com>
2016-02-19 12:30:09 +0800

    翻版更新

---
+++ MySinLangTest/src/com/example/mysinlangtest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mysinlangtest;
+
+import com.sina.weibo.sdk.auth.AuthInfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	public AuthInfo mAuthInfo;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	public void creatWeoBo(){
+		mAuthInfo = new AuthInfo(this, Constants.APP_KEY, 
+				Constants.REDIRECT_URL, Constants.SCOPE);
+        mAuthInfo.getAuthBundle();
+	}
+}
